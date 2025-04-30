720748a72c619916109822a6560f723ceed54656
Serafine-design <serafino.shinn@gmail.com>
2025-04-30 14:03:45 +1200

    import sqlite database

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print('hello world')
-print('why not work')
+import sqlite3
+
+db = sqlite3.connect('music.db')
+cursor = db.cursor()
+sql = "SELECT * FROM music;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+for music in results:
+    print(music)
+
+db.close()
