3847be48997d3c24ed449379219de307f15e63bd
Serafine-design <serafino.shinn@gmail.com>
2025-05-01 21:01:11 +1200

    finished adding find song using artist

--- artist.py
+++ artist.py
@@ -2,34 +2,27 @@ import sqlite3
 
 def find_song_using_artist():
     '''Enter genre to find song'''
+    artistcheck = 0
     Database = 'music.db'
     number = 1
     db = sqlite3.connect(Database)
     cursor = db.cursor()
-    sql = "SELECT * FROM music;"
-    sqlartist = "SELECT * FROM artist"
+    sql = "SELECT * From music JOIN artist ON music.artist_id = artist.artist_id;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    cursor.execute(sqlartist)
-    resultartist = cursor.fetchall()
 
-    print(results)
-    print(resultartist)
-
-    userartist = input('Enter your artist of choice please.')
+    userartist = input('Enter your artist of choice please.\n').upper()
     while True:
-        for artist in resultartist:
-            if userartist == artist[1]:
-                for music in results:
-                    if music[3] == artist[0]:
-                        print(music[1])
-                break
-            else:
-                print('no such artist')
-
-
-                
+        print('')
+        for artist in results:
+            if userartist == artist[5].upper():
+                print(artist[1])
+                artistcheck = artistcheck + 1
         
+        if artistcheck == 0:
+            userartist =  input('Not a valid artist please make sure you have the correct spelling.\n').upper()
+        else:
+            break
     db.close()
 
 find_song_using_artist()
--- maincode.py
+++ maincode.py
@@ -41,10 +41,37 @@ def find_song_using_genre():
         
     db.close()
 
+def find_song_using_artist():
+    '''Enter genre to find song'''
+    artistcheck = 0
+    Database = 'music.db'
+    number = 1
+    db = sqlite3.connect(Database)
+    cursor = db.cursor()
+    sql = "SELECT * From music JOIN artist ON music.artist_id = artist.artist_id;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    userartist = input('Enter your artist of choice please.\n').upper()
+    while True:
+        print('')
+        for artist in results:
+            if userartist == artist[5].upper():
+                print(artist[1])
+                artistcheck = artistcheck + 1
+        
+        if artistcheck == 0:
+            userartist =  input('Not a valid artist please make sure you have the correct spelling.\n').upper()
+        else:
+            break
+    db.close()
+
 # Main code
 
-userinput = int(input('1 or 2'))
+userinput = int(input('1 or 2 or 3'))
 if userinput == 1:
     print_all_songs()
 elif userinput == 2:
     find_song_using_genre()
+elif userinput == 3:
+    find_song_using_artist()
