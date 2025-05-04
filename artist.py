ee5560b1631d859495ceda0683b80f5e4de220a6
Serafine-design <serafino.shinn@gmail.com>
2025-05-04 19:25:30 +1200

    fixed more errors that occured because of new database

--- artist.py
+++ artist.py
@@ -2,31 +2,30 @@ import sqlite3
 
 def find_song_using_artist():
     '''Enter genre to find song'''
+    check = 0
     artistcheck = 0
     Database = 'musicwithstream.db'
     number = 1
     db = sqlite3.connect(Database)
     cursor = db.cursor()
-    sql = "SELECT * From music JOIN artist ON music.artist_id = artist.artist_id;"
-    sql2 = "SELECT * FROM music JOIN genre ON music.genre_id = genre.genre_id;"
+    sql = "Select * from music INNER JOIN artist on music.artist_id = artist.artist_id INNER JOIN genre on music.genre_id = genre.genre_id;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    cursor.execute(sql2)
-    result2 = cursor.fetchall()
     userartist = input('Enter your artist of choice please.\n').upper()
     while True:
         print('')
         for artist in results:
             if userartist == artist[6].upper():
-                for genre in result2:
-                    if genre[1] == artist[1]:
-                        print(f"{artist[1]:<30}{genre[5]}")
-                        artistcheck = artistcheck + 1
-        
-        if artistcheck == 0:
-            userartist =  input('Not a valid artist please make sure you have the correct spelling.\n').upper()
-        else:
+                check = check + 1
+        if check != 0:
+            print('Name                                              Genre               Streams on Spotify')
+            for artist in results:
+                if userartist == artist[6].upper():
+                    print(f"{artist[1]:<50}{artist[6]:<20}{artist[4]}")
             break
+        else:
+            userartist = input('Not a valid artist please make sure you have the correct spelling.\n').upper()
+            check = 0
     db.close()
 
 find_song_using_artist()
--- maincode.py
+++ maincode.py
@@ -44,31 +44,30 @@ def find_song_using_genre():
 
 def find_song_using_artist():
     '''Enter genre to find song'''
+    check = 0
     artistcheck = 0
     Database = 'musicwithstream.db'
     number = 1
     db = sqlite3.connect(Database)
     cursor = db.cursor()
-    sql = "SELECT * From music JOIN artist ON music.artist_id = artist.artist_id;"
-    sql2 = "SELECT * FROM music JOIN genre ON music.genre_id = genre.genre_id;"
+    sql = "Select * from music INNER JOIN artist on music.artist_id = artist.artist_id INNER JOIN genre on music.genre_id = genre.genre_id;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    cursor.execute(sql2)
-    result2 = cursor.fetchall()
     userartist = input('Enter your artist of choice please.\n').upper()
     while True:
         print('')
         for artist in results:
-            if userartist == artist[5].upper():
-                for genre in result2:
-                    if genre[1] == artist[1]:
-                        print(f"{artist[1]:<30}{genre[5]}")
-                        artistcheck = artistcheck + 1
-        
-        if artistcheck == 0:
-            userartist =  input('Not a valid artist please make sure you have the correct spelling.\n').upper()
-        else:
+            if userartist == artist[6].upper():
+                check = check + 1
+        if check != 0:
+            print('Name                                              Genre               Streams on Spotify')
+            for artist in results:
+                if userartist == artist[6].upper():
+                    print(f"{artist[1]:<50}{artist[6]:<20}{artist[4]}")
             break
+        else:
+            userartist = input('Not a valid artist please make sure you have the correct spelling.\n').upper()
+            check = 0
     db.close()
 
 
