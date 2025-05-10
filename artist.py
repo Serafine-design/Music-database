05375c9432ece33efbfc944ea884f31c401d1685
Serafine-design <serafino.shinn@gmail.com>
2025-05-10 17:05:13 +1200

    added option to exit when searching using artist

--- artist.py
+++ artist.py
@@ -11,21 +11,23 @@ def find_song_using_artist():
     sql = "Select * from music INNER JOIN artist on music.artist_id = artist.artist_id INNER JOIN genre on music.genre_id = genre.genre_id;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    userartist = input('Enter your artist of choice please.\n').upper()
+    userartist = input('Enter your artist of choice please or type exit to exit.\n').upper()
     while True:
-        print('')
-        for artist in results:
-            if userartist == artist[6].upper():
-                check = check + 1
-        if check != 0:
-            print('Name                                              Genre               Streams on Spotify')
-            for artist in results:
-                if userartist == artist[6].upper():
-                    print(f"{artist[1]:<50}{artist[6]:<20}{artist[4]}")
+        if userartist == 'EXIT':
             break
         else:
-            userartist = input('Not a valid artist please make sure you have the correct spelling.\n').upper()
-            check = 0
+            for artist in results:
+                if userartist == artist[6].upper():
+                    check = check + 1
+            if check != 0:
+                print('Name                                              Genre               Streams on Spotify')
+                for artist in results:
+                    if userartist == artist[6].upper():
+                        print(f"{artist[1]:<50}{artist[6]:<20}{artist[4]}")
+                break
+            else:
+                userartist = input('Not a valid artist please make sure you have the correct spelling.\n').upper()
+                check = 0
     db.close()
 
 find_song_using_artist()
--- maincode.py
+++ maincode.py
@@ -126,19 +126,21 @@ def find_song_using_artist():
     results = cursor.fetchall()
     userartist = input('Enter your artist of choice please.\n').upper()
     while True:
-        print('')
-        for artist in results:
-            if userartist == artist[6].upper():
-                check = check + 1
-        if check != 0:
-            print('Name                                              Genre               Streams on Spotify')
-            for artist in results:
-                if userartist == artist[6].upper():
-                    print(f"{artist[1]:<50}{artist[6]:<20}{artist[4]}")
+        if userartist == 'EXIT':
             break
         else:
-            userartist = input('Not a valid artist please make sure you have the correct spelling.\n').upper()
-            check = 0
+            for artist in results:
+                if userartist == artist[6].upper():
+                    check = check + 1
+            if check != 0:
+                print('Name                                              Genre               Streams on Spotify')
+                for artist in results:
+                    if userartist == artist[6].upper():
+                        print(f"{artist[1]:<50}{artist[6]:<20}{artist[4]}")
+                break
+            else:
+                userartist = input('Not a valid artist please make sure you have the correct spelling.\n').upper()
+                check = 0
     db.close()
 
 def print_random_song():
@@ -193,7 +195,7 @@ def enter_song_to_print():
                         break
                     else:    
                         check = check + 1
-
+    db.close()
 
 
 def main_code():
